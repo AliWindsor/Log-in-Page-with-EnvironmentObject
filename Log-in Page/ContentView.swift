4411d30b7faf69d74502ca278f8975b3a2262498
AliWindsor <aliwindsor@gmail.com>
2021-06-03 13:39:01 +0100

    Initial Commit

---
+++ Log-in Page/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Log-in Page
+//
+//  Created by Alicia Windsor on 03/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Log-in Page/Log_in_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Log_in_PageApp.swift
+//  Log-in Page
+//
+//  Created by Alicia Windsor on 03/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Log_in_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
